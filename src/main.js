2acbf56ec21d5cf07edd6bb7d217ab6084aff75b
YuliyaT8 <155116594+YuliyaT8@users.noreply.github.com>
2024-06-30 22:19:17 +0300

    ***

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
-// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
-// Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
-
 import { getImages } from './js/pixabay-api';
-import { showLoader, imagesTemplate, hideLoader } from './js/render-functions';
+import {
+  imagesTemplate,
+  showLoader,
+  hideLoader,
+  showLoadMore,
+  hideLoadMore,
+  checkEndPages,
+} from './js/render-functions';
+import { refs } from './js/refs';
+import { skipOldElement } from './js/scroll';
 
-export const refs = {
-  form: document.querySelector('.form'),
-  input: document.querySelector('.input'),
-  button: document.querySelector('button'),
-  gallery: document.querySelector('.gallery'),
-  loader: document.querySelector('.loader'),
-};
+let inputValue = '';
+let currentPage = 1;
+let perPage = 15;
+let maxPage = 1;
 
-refs.form.addEventListener('submit', e => {
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+refs.form.addEventListener('submit', async e => {
   e.preventDefault();
-  const inputValue = e.target.elements.text.value.trim();
+  inputValue = e.target.elements.text.value.trim();
+  currentPage = 1;
+
+  hideLoadMore();
+
   if (inputValue === '') {
     refs.gallery.innerHTML = ' ';
     iziToast.warning({
@@ -27,40 +42,78 @@ refs.form.addEventListener('submit', e => {
       position: 'topRight',
       displayMode: 'once',
     });
+    refs.form.reset();
     return;
   }
 
   showLoader();
+
   refs.gallery.innerHTML = ' ';
-  getImages(inputValue)
-    .then(data => {
-      if (data.hits.length === 0) {
-        iziToast.error({
-          message:
-            'Sorry, there are no images matching your search query. Please try again!',
-          layout: 2,
-          displayMode: 'once',
-          backgroundColor: '#ef4040',
-          progressBarColor: '#B51B1B',
-          position: 'topRight',
-        });
-        hideLoader();
-        refs.form.reset();
-        return;
-      }
-      hideLoader();
-      refs.form.reset();
-      imagesTemplate(data.hits);
-    })
-    .catch(err => {
+
+  try {
+    const data = await getImages(inputValue, currentPage, perPage);
+    maxPage = Math.ceil(data.totalHits / perPage);
+    if (maxPage === 0) {
       iziToast.error({
-        title: 'Error',
-        message: `${err}`,
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
         layout: 2,
         displayMode: 'once',
         backgroundColor: '#ef4040',
         progressBarColor: '#B51B1B',
         position: 'topRight',
       });
+      refs.form.reset();
+      hideLoader();
+      return;
+    }
+
+    hideLoader();
+    refs.form.reset();
+
+    imagesTemplate(data.hits);
+    lightbox.refresh();
+    showLoadMore();
+  } catch (error) {
+    iziToast.error({
+      title: 'Error',
+      message: `${error}`,
+      layout: 2,
+      displayMode: 'once',
+      backgroundColor: '#ef4040',
+      progressBarColor: '#B51B1B',
+      position: 'topRight',
     });
+  }
+});
+
+refs.loadMoreBtn.addEventListener('click', async () => {
+  hideLoadMore();
+  showLoader();
+
+  try {
+    currentPage++;
+
+    const data = await getImages(inputValue, currentPage, perPage);
+
+    if (data.hits.length !== 0) {
+      imagesTemplate(data.hits);
+      lightbox.refresh();
+      hideLoader();
+    }
+    checkEndPages(currentPage, maxPage);
+    skipOldElement();
+  } catch (error) {
+    refs.gallery.innerHTML = ' ';
+
+    iziToast.error({
+      title: 'Error',
+      message: `${error}`,
+      layout: 2,
+      displayMode: 'once',
+      backgroundColor: '#ef4040',
+      progressBarColor: '#B51B1B',
+      position: 'topRight',
+    });
+  }
 });
